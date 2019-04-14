d3f5da48cd001fad25273a238e3236411a0baaf6
Mukur Panchani <panchanimukur@gmail.com>
2019-04-15 02:47:38 +0530

    Do basic error handling and flask DEV mode

--- flaskapp.py
+++ flaskapp.py
@@ -1,6 +1,7 @@
 import collector
 from flask import Flask
 from flask import request
+from flask import render_template
 from collections import Counter
 import requests
 app = Flask(__name__)
@@ -9,30 +10,28 @@ app = Flask(__name__)
 def hello_world():
   return 'Hello from Flask!'
 
-@app.route('/countme/<input_str>')
-def count_me(input_str):
-    input_counter = Counter(input_str)
-    response = []
-    for letter, count in input_counter.most_common():
-        response.append('"{}": {}'.format(letter, count))
-    return '<br>'.join(response)
-
-@app.route('/attemptSlcmLogin', methods=["GET","POST"])
-def attemptSlcmLogin():
+@app.route('/go', methods=["GET","POST"])
+def go():
     if request.method == 'POST':
-        col = collector.Collector(request.form['username'], request.form['password'])
-        col.makeReq()
-        return 'dOnEzO'
+        print(request.form)
+        try:
+            if request.form.get('username') == None or request.form.get('password') == None:
+                return '{"error" : "missing parameter", "code" : "00"}'
+            col = collector.Collector(request.form['username'], request.form['password'])
+            col.makeReq()
+            if col.errorDuringExtraction == True:
+                return '{"error" : "server side error", "code" : "10"}'
+            return '<h1 style="text-align: center; font-family: consolas">POST SUCCESSFUL</h1>'
+        except:
+            return '{"error" : "server side error" , "code" : "11"}'
     
     if request.method == 'GET':
-        col = collector.Collector('160905032', 'Eybitches')
-        col.makeReq()
-        return '<h1 style="text-align: center; font-family: consolas">WOKE</h1>'
+        return render_template('slcmgo_get_response.html', bois_ip=request.remote_addr)
 
-@app.route('/test')
-def test():
+@app.route('/testGoPost')
+def testSlcmGo():
     dictToSend = {'username':'160905032', 'password':'Eybitches'}
-    res = requests.post('http://localhost:5000/attemptSlcmLogin', data=dictToSend)
+    res = requests.post('http://localhost:5000/go', data=dictToSend)
     return res.text
 
 if __name__ == '__main__':
