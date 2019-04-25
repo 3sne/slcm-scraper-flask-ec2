5a7ab897c8dc099af0a1965d520e8ae8f6ae3393
Mukur Panchani <panchanimukur@gmail.com>
2019-04-25 21:53:38 +0530

    Improve get and post code size

--- flaskapp.py
+++ flaskapp.py
@@ -7,6 +7,7 @@ from flask import jsonify
 from collections import Counter
 import requests
 app = Flask(__name__)
+app.secret_key = b'\xad,O\t\xcc:}\xae\xc846\x83K]\xe0\xd7\xb8)4\x08\xdc*i\xe5\x9f\x0c\x94\xc8\x1f\xbd\x01\x95'
 
 @app.route('/')
 def hello_world():
@@ -15,43 +16,41 @@ def hello_world():
 @app.route('/go', methods=["GET","POST"])
 def go():
     resData = {}
+    ext_uname = ''
+    ext_pw = ''
     if request.method == 'POST':
-        print(request.form)
         try:
-            if request.form.get('username') == None or request.form.get('password') == None:
-                return '{"error" : "missing parameter", "code" : "00"}'
-            if request.form.get('api_call') != 'True':
-                return '{"error" : "missing parameter", "code" : "01"}'
-            col = collector.Collector(request.form['username'], request.form['password'])
-            col.makeReq()
-            if col.errorDuringExtraction == True:
-                return '{"error" : "server side error", "code" : "10"}'
-            return '<h1 style="text-align: center; font-family: consolas">POST SUCCESSFUL</h1>'
+            if request.form.get('slcm_username') and request.form.get('slcm_password'):
+                ext_uname = request.form.get('slcm_username')
+                ext_pw = request.form.get('slcm_password')
         except:
-            return '{"error" : "server side error" , "code" : "11"}'
-    
-    if request.method == 'GET':
+            resData["code"] = "201"
+            return jsonify(resData)
+    elif request.method == 'GET':
         try:
             if request.args.get('username') and request.args.get('password'):
-                col = collector.Collector(request.args['username'], request.args['password'])
-                col.makeReq()
-                if col.loginError:
-                    resData["code"] = "100"
-                    return jsonify(resData)
-                if col.collectionError:
-                    resData["code"] = "101"
-                    return jsonify(resData)
-                if col.errorDuringExtraction:
-                    resData["code"] = "102"
-                    return jsonify(resData)
-                resData["code"] = "666"
-                resData["data"] = col.attendanceData
-                return jsonify(resData)
+                ext_uname = request.args.get('username')
+                ext_pw = request.args.get('password')
             else:
                 return render_template('slcmgo_get_response.html', bois_ip=request.remote_addr)
         except:
             resData["code"] = "200"
             return jsonify(resData)
+
+    col = collector.Collector(ext_uname, ext_pw)
+    col.makeReq()
+    if col.loginError:
+        resData["code"] = "100"
+        return jsonify(resData)
+    if col.collectionError:
+        resData["code"] = "101"
+        return jsonify(resData)
+    if col.errorDuringExtraction:
+        resData["code"] = "102"
+        return jsonify(resData)
+    resData["code"] = "666"
+    resData["data"] = col.attendanceData
+    return jsonify(resData)
 
 @app.route('/adminFetch', methods=["GET"])
 def adminFetch():
@@ -73,9 +72,13 @@ def adminFetch():
             return '{"error" : "adminFetch main if error" , "code" : "-1"}'
 
 @app.route('/testGoPost')
-def testSlcmGo():
-    dictToSend = {'username':'160905032', 'password':'Eybitches', 'api_call':'True'}
-    res = requests.post('http://localhost:5000/go', data=dictToSend)
+def testGoPost():
+    res = requests.post('http://localhost:5000/go', data={'slcm_username':'160905032', 'slcm_password':'Eybitches', 'a_call':'true'})
+    return res.text
+
+@app.route('/testGoGet')
+def testGoGet():
+    res = requests.get('http://localhost:5000/go?username=160905032&password=Eybitches')
     return res.text
 
 if __name__ == '__main__':
